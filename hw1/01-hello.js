a35c0a56b8d1574068511956d79d89582783dfbd
Moses Karemera <ndagijim22@up.edu>
2023-10-06 15:02:47 -0700

    Finished all

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(
+    `My name is Moses and I will taking CS 463/563: Intro to Web Development this quarter.This is my first quarter as a graduate student at PSU. I'm taking this class to increase my skills in webDev.I'm from Portland and I have been living here for about 9 years now. I love Portland city because people here are friendly.`
+  );
 }
