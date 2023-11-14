aeb768bfe4cf7a0fee2da7be5e4d513f90f65327
mkaremer <mkaremer@pdx.edu>
2023-11-13 16:00:07 -0800

    Finished all

--- hw3/02-form.js
+++ hw3/02-form.js
@@ -1,1 +1,54 @@
-// Add your code here
+//Add your code here
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".form-container");
+
+  // This function will handle the form submission
+  function handleFormSubmit(e) {
+    // Prevent the default form submission
+    e.preventDefault();
+
+    // store form data in an object
+    const formData = new FormData(form);
+    const data = {};
+    formData.forEach((value, key) => {
+      data[key] = value;
+    });
+
+    // check if all fields are filled
+    let isFormValid = true;
+    for (let key in data) {
+      if (!data[key]) {
+        isFormValid = false;
+        break;
+      }
+    }
+
+    // If the form is valid, format and log the data to the console
+    if (isFormValid) {
+      console.log("========Form Submission========");
+      console.log(`    Name: ${data.name}`);
+      console.log(`    Username: ${data.username}`);
+      console.log(`    Email: ${data.email}`);
+      console.log(`    Date of Birth: ${dateFormat(data.dob)}`);
+      console.log(`    Preferred Pronouns: ${data.pronouns}`);
+    } else {
+      console.log("Cannot submit, all fields are required.");
+    }
+  }
+  //convert date of birth to a desired format
+  function dateFormat(date) {
+    const dob = new Date(date);
+    const dobFormatted = dob.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    return dobFormatted;
+  }
+
+  // Add the event listener for the 'submit' event
+  form.addEventListener("submit", handleFormSubmit);
+
+  // Reset form to ensure it's empty on load
+  form.reset();
+});
--- hw3/03-starships.js
+++ hw3/03-starships.js
@@ -1,58 +1,143 @@
-const fetchData = (url) => {
+const fetchData = async (url) => {
   // REtrieve the data from the API
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 };
+// Fetch starships data and initialize the starships variable
+fetchData("https://swapi.dev/api/starships/").then((data) => {
+  starships = data;
+});
 
 const createSpaceshipComponent = (spaceship) => {
-  const container = document.createElement('section'); // do not modify this line
+  const container = document.createElement("section");
+  container.className = "starship-component";
 
-  return container; // do not modify this line
+  const div = document.createElement("div");
+  div.className = "top-div";
+  container.appendChild(div);
+
+  //Spaceship name
+  const nameHeading = document.createElement("h2");
+  nameHeading.textContent = spaceship.name;
+  nameHeading.className = "space-name";
+  div.appendChild(nameHeading);
+
+  //Spaceship Cost in credits
+  const costPara = document.createElement("h3");
+  const cost = parseInt(spaceship.cost_in_credits, 10);
+
+  // Check if the cost is a number
+  if (!isNaN(cost)) {
+    const formattedCost = cost.toLocaleString();
+    costPara.textContent = `${formattedCost} credits `;
+  } else {
+    costPara.textContent = "Cost not available";
+  }
+
+  costPara.className = "cost-credits";
+  div.appendChild(costPara);
+
+  //spaceship manufaturer
+  const manufacturerPara = document.createElement("p");
+  manufacturerPara.textContent = `Manufactured by ${spaceship.manufacturer}`;
+  manufacturerPara.className = "manufacturer";
+  container.appendChild(manufacturerPara);
+
+  //spaceship max atmosphering speed
+  const speedPara = document.createElement("p");
+  speedPara.className = "max-speed";
+
+  const speedValue = document.createElement("div");
+  speedValue.style.fontWeight = "bold";
+  speedValue.textContent = spaceship.max_atmosphering_speed;
+  speedPara.appendChild(speedValue);
+
+  // Create a span for the "max atmosphering speed" label
+  const speedLabel = document.createElement("span");
+  speedLabel.textContent = "Max atmosphering speed";
+  speedPara.appendChild(speedLabel);
+  container.appendChild(speedPara);
+
+  // spaceship cargo capacity
+  const capacityPara = document.createElement("p");
+  capacityPara.className = "cargo-capacity";
+
+  // Create a div for the value and make it bold
+  const capacityValue = document.createElement("div");
+  capacityValue.style.fontWeight = "bold";
+  capacityValue.textContent = parseInt(
+    spaceship.cargo_capacity
+  ).toLocaleString();
+  capacityPara.appendChild(capacityValue);
+
+  // Create a span for the "Cargo capacity" label
+  const capacityLabel = document.createElement("span");
+  capacityLabel.textContent = "Cargo capacity";
+  capacityPara.appendChild(capacityLabel);
+  container.appendChild(capacityPara);
+
+  return container;
 };
 
-const main = document.getElementsByTagName('main')[0];
+const main = document.getElementsByTagName("main")[0];
 
 const filterStarships = (input) => {
   // Return an array with all ships that have less than 10 passengers with more than one crew member
+  return input.filter(
+    (ship) => parseInt(ship.passengers, 10) < 10 && parseInt(ship.crew, 10) > 1
+  );
 };
 
 const reduceStarships = (input) => {
   // Return a String of the cost to purchase all ships in the input array
-  const totalCost = 0;
+  const totalCost = input.reduce((acc, ship) => {
+    const cost = isNaN(parseInt(ship.cost_in_credits, 10))
+      ? 0
+      : parseInt(ship.cost_in_credits, 10);
+    return acc + cost;
+  }, 0);
 
   return `The cost of all starships is ${totalCost.toLocaleString()} credits`;
 };
 
 // do not modify the code below
-let displayAllButton = document.getElementById('all');
-displayAllButton.addEventListener('click', () => {
+let displayAllButton = document.getElementById("all");
+displayAllButton.addEventListener("click", () => {
   displayShipComponents(starships);
 });
 
-let filterButton = document.getElementById('filter');
-filterButton.addEventListener('click', () => {
+let filterButton = document.getElementById("filter");
+filterButton.addEventListener("click", () => {
   const filteredShips = filterStarships(starships);
   displayShipComponents(filteredShips);
 });
 
-let reduceButton = document.getElementById('reduce');
-reduceButton.addEventListener('click', () => {
+let reduceButton = document.getElementById("reduce");
+reduceButton.addEventListener("click", () => {
   const totalCost = reduceStarships(starships);
   displayText(totalCost);
 });
 
 const clearAndReset = () => {
-  let app = document.getElementById('results');
+  let app = document.getElementById("results");
   app.remove();
-  app = document.createElement('div');
-  app.id = 'results';
-  app.style.display = 'flex';
-  app.style.flexWrap = 'wrap';
-  app.style.justifyContent = 'center';
+  app = document.createElement("div");
+  app.id = "results";
+  app.style.display = "flex";
+  app.style.flexWrap = "wrap";
+  app.style.justifyContent = "center";
   main.append(app);
 };
 
 const displayShipComponents = (starships) => {
   clearAndReset();
-  let app = document.getElementById('results');
+  let app = document.getElementById("results");
   for (const ship in starships) {
     const shipComponent = createSpaceshipComponent(starships[ship]);
     app.appendChild(shipComponent);
@@ -61,11 +146,11 @@ const displayShipComponents = (starships) => {
 
 const displayText = (text) => {
   clearAndReset();
-  let app = document.getElementById('results');
-  let paragraph = document.createElement('p');
+  let app = document.getElementById("results");
+  let paragraph = document.createElement("p");
   paragraph.textContent = text;
-  paragraph.style.backgroundColor = 'white';
-  paragraph.style.borderRadius = '10px';
-  paragraph.style.padding = '30px';
+  paragraph.style.backgroundColor = "white";
+  paragraph.style.borderRadius = "10px";
+  paragraph.style.padding = "30px";
   app.appendChild(paragraph);
 };
